b240829c33e0d68a3aa9b98ab59841cf91643eea
yule10s <yule10s@192.168.0.3>
2018-08-30 22:58:02 +0900

    User.javaの作成

--- src/jp/co/yule/model/User.java
+++ src/jp/co/yule/model/User.java
@@ -1,5 +1,21 @@
 package jp.co.yule.model;
+import java.io.Serializable;
 
-public class User {
+public class User implements Serializable {
+	private String name;
+	private String pass;
+
+	public User(){}
+	public User(String name , String pass) {
+		this.name = name;
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public String getPass() {
+		return pass;
+	}
 
 }
